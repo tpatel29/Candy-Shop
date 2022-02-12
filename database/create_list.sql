d76eb4d72e3fa5dfb007686dfd7ad7172621aedb
Hassan <hassan051999@gmail.com>
2022-02-12 12:49:49 -0500

    added sql file

---
+++ database/create_list.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `Products` (
+    `id` int AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL,
+    `price` DECIMAL(12,2) DEFAULT 0,
+    `category` VARCHAR(100) NOT NULL,
+    `flavor` VARCHAR(100) NOT NULL
+)
