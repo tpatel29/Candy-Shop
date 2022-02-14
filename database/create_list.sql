bb9885b3f44381772099851e13edefb253ac3d10
Hassan <hassan051999@gmail.com>
2022-02-13 23:34:07 -0500

    added products into table

--- database/create_list.sql
+++ database/create_list.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `Products` (
     `id` int AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(100) NOT NULL,
+    `brand` VARCHAR(100) NOT NULL,
     `price` DECIMAL(12,2) DEFAULT 0,
-    `category` VARCHAR(100) NOT NULL,
+    `size` VARCHAR(100) NOT NULL,
     `flavor` VARCHAR(100) NOT NULL
 )
