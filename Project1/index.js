e84747478cd90811c5f6bf849fd0efeba786a956
pa284 <85532635+pa284@users.noreply.github.com>
2022-02-18 22:59:55 -0500

    updated index.js

--- Project1/index.js
+++ Project1/index.js
@@ -1,77 +1,186 @@
-const track = document.querySelector('.carousel__track');
-const slides = Array.from(track.children);
-const nextButton = document.querySelector('.carousel__button--right');
-const prevButton = document.querySelector('.carousel__button--left');
-const dotsNav = document.querySelector('.carousel__nav');
-const dots = Array.from(dotsNav.children);
+let rightArrow = document.querySelector("#carousel-1 .right-arrow");
+let leftArrow = document.querySelector("#carousel-1 .left-arrow");
 
-const slideWidth = slides[0].getBoundingClientRect().width;
+let screenDisplay = document.querySelectorAll("#carousel-1 .carousel-screen");
+let numOfScreens = screenDisplay.length;
 
-const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + 'px';
-};
-slides.forEach(setSlidePosition);
+let circles = document.querySelectorAll("#carousel-1 .circle-container .circle");
 
-const moveToSlide = (track, currentSlide, targetSlide) => {
-    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-    currentSlide.classList.remove('current-slide');
-    targetSlide.classList.add('current-slide');
-};
+let currentScreen = 0;
 
-const updateDots = (currentDot, targetDot) => {
-    currentDot.classList.remove('current-slide');
-    targetDot.classList.add('current-slide');
-}
+let inAnimation = false;
 
-const hideArrows = (slides, prevButton, nextButton, targetIndex) => {
-    if(targetIndex === 0){
-        prevButton.classList.add('is-hidden');
-        nextButton.classList.remove('is-hidden');
-    } else if(targetIndex === slides.length - 1){
-        prevButton.classList.remove('is-hidden');
-        nextButton.classList.add('is-hidden');
-    } else {
-        prevButton.classList.remove('is-hidden');
-        nextButton.classList.remove('is-hidden');
+let animationTime = 500;
+
+sortPosition(screenDisplay[currentScreen], screenDisplay[currentScreen-1], screenDisplay[currentScreen+1]);
+highlightCircle(circles[0]);
+
+rightArrow.addEventListener("click", () => {
+    startAnimation("right");
+});
+
+leftArrow.addEventListener("click", () => {
+    startAnimation("left");
+});
+
+function startAnimation(direction){
+    if(!inAnimation){
+        inAnimation = true;
+        if(direction === "right"){
+            moveRight();
+            highlightCircle(circles[currentScreen+1], "right");
+        } else if(direction === "left"){
+            moveLeft();
+            highlightCircle(circles[currentScreen-1], "left");
+        } else {
+            inAnimation = false;
+            return
+        }
     }
 }
-prevButton.addEventListener('click', e => {
-    const currentSlide = track.querySelector('.current-slide');
-    const prevSlide = currentSlide.previousElementSibling;
-    const currentDot = dotsNav.querySelector('.current-slide');
-    const previousDot = currentDot.previousElementSibling;
-    const prevIndex = slides.findIndex(slide => slide === prevSlide);
 
-    moveToSlide(track, currentSlide, prevSlide);
-    updateDots(currentDot, previousDot);
-    hideArrows(slides, prevButton, nextButton, prevIndex);
-});
+function moveRight(){
+    if(currentScreen < numOfScreens - 1){
+        toLeft(screenDisplay[currentScreen]);
+        comeRight(screenDisplay[currentScreen + 1]);
+        setTimeout(() => {
+            inAnimation = false;
+            currentScreen++;
+            sortPosition(screenDisplay[currentScreen], screenDisplay[currentScreen-1], screenDisplay[currentScreen+1]);
+        }, animationTime)
+    } else {
+        toLeft(screenDisplay[currentScreen]);
+        comeRight(screenDisplay[0]);
+        setTimeout(() => {
+            inAnimation = false;
+            currentScreen = 0;
+            sortPosition(screenDisplay[currentScreen], screenDisplay[currentScreen-1], screenDisplay[currentScreen+1]);
+        }, animationTime)
+    }
+}
 
-nextButton.addEventListener('click', e => {
-    const currentSlide = track.querySelector('.current-slide');
-    const nextSlide = currentSlide.nextElementSibling;
-    const currentDot = dotsNav.querySelector('.current-slide');
-    const nextDot = currentDot.nextElementSibling;
-    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+function moveLeft(){
+    if(currentScreen > 0){
+        toRight(screenDisplay[currentScreen]);
+        comeLeft(screenDisplay[currentScreen - 1]);
+        setTimeout(() => {
+            inAnimation = false;
+            currentScreen--;
+            sortPosition(screenDisplay[currentScreen], screenDisplay[currentScreen-1], screenDisplay[currentScreen+1]);
+        }, animationTime)
+    } else {
+        toRight(screenDisplay[currentScreen]);
+        comeLeft(screenDisplay[numOfScreens - 1]);
+        setTimeout(() => {
+            inAnimation = false;
+            currentScreen = numOfScreens - 1;
+            sortPosition(screenDisplay[currentScreen], screenDisplay[currentScreen-1], screenDisplay[currentScreen+1]);
+        }, animationTime)
+    }
+}
 
-    moveToSlide(track, currentSlide, nextSlide);
-    updateDots(currentDot, nextDot);
-    hideArrows(slides, prevButton, nextButton, nextIndex);
-});
+function toLeft(screen) {
+    screen.style.animation = "toLeft 0.5s ease-in-out forwards";
+    setTimeout(() => {
+        screen.style.animation = "";
+    }, animationTime)
+}
 
-dotsNav.addEventListener('click', e => {
-    const targetDot = e.target.closest('button');
-    if(!targetDot) return;
+function toRight(screen) {
+    screen.style.animation = "toRight 0.5s ease-in-out forwards";
+    setTimeout(() => {
+        screen.style.animation = "";
+    }, animationTime)
+}
 
-    const currentSlide = track.querySelector('.current-slide');
-    const currentDot = dotsNav.querySelector('.current-slide');
-    const targetIndex = dots.findIndex(dot => dot === targetDot);
-    const targetSlide = slides[targetIndex];
+function comeRight(screen) {
+    screen.style.animation = "comeRight 0.5s ease-in-out forwards";
+    setTimeout(() => {
+        screen.style.animation = "";
+    }, animationTime)
+}
 
-    moveToSlide(track, currentSlide, targetSlide);
-    updateDots(currentDot, targetDot);
-    hideArrows(slides, prevButton, nextButton, targetIndex);
-});
+function comeLeft(screen) {
+    screen.style.animation = "comeLeft 0.5s ease-in-out forwards";
+    setTimeout(() => {
+        screen.style.animation = "";
+    }, animationTime)
+}
+
+function sortPosition(mainScreen, leftScreen, rightScreen){
+    if(rightScreen === undefined){
+        rightScreen = screenDisplay[0];
+    }
+    if(leftScreen === undefined){
+        leftScreen = screenDisplay[numOfScreens-1];
+    }
+
+    screenDisplay.forEach(screen => {
+        if(screen === mainScreen){
+            screen.style.display = "block";
+            screen.style.left = "0px";
+        } else if(screen === leftScreen){
+            screen.style.display = "block";
+            screen.style.left = "-100%";
+        } else if(screen === rightScreen){
+            screen.style.display = "block";
+            screen.style.left = "100%";
+        } else {
+            screen.style.display = "none";
+        }
+    })
+}
+
+circles.forEach(circle => {
+    circle.addEventListener("click", event => {
+        if(!inAnimation){
+            let circlesArray = Array.prototype.slice.call(circles);
+            let circleIndex = circlesArray.indexOf(event.target);
+            highlightCircle(event.target);
+            if(circleIndex > currentScreen){
+                changeScreenCircle(circleIndex, "right");
+            } else if(circleIndex < currentScreen){
+                changeScreenCircle(circleIndex, "left");
+            }
+        }
+    })
+})
+
+function changeScreenCircle(circleIndex, direction){
+    inAnimation = true;
+    if(direction === "right"){
+        sortPosition(screenDisplay[currentScreen], screenDisplay[currentScreen-1], screenDisplay[circleIndex]);
+        toLeft(screenDisplay[currentScreen]);
+        comeRight(screenDisplay[circleIndex]);
+    } else if(direction === "left"){
+        sortPosition(screenDisplay[currentScreen], screenDisplay[circleIndex], screenDisplay[currentScreen + 1]);
+        toRight(screenDisplay[currentScreen]);
+        comeLeft(screenDisplay[circleIndex]);
+    } else {
+        inAnimation = false;
+        return
+    }
+    setTimeout(() => {
+        inAnimation = false;
+        currentScreen = circleIndex;
+        sortPosition(screenDisplay[currentScreen], screenDisplay[currentScreen-1], screenDisplay[currentScreen+1]);
+    }, animationTime)
+}
+
+function highlightCircle(circleSelect, direction){
+    if(circleSelect === undefined && direction === "right"){
+        circleSelect = circles[0];
+    } else if(circleSelect === undefined && direction === "left"){
+        circleSelect = circles[numOfScreens-1]
+    }
+    circles.forEach(circle => {
+        if(circle === circleSelect){
+            circle.classList.add("circle-fill");
+        } else {
+            circle.classList.remove("circle-fill");
+        }
+    })
+}
 
 
 
