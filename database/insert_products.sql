6461905a9a23794425f419cef8ee3fd79175ab5f
Hassan <hassan051999@gmail.com>
2022-02-19 22:39:22 -0500

    Corrected sql file to include names. Removed some items to make it 11 items. I also added all of the images of every item in the Candy Pictures folder.

--- database/insert_products.sql
+++ database/insert_products.sql
@@ -1,4 +1,5 @@
 INSERT INTO Products (
+    name,
     brand,
     price,
     size,
@@ -6,122 +7,79 @@ INSERT INTO Products (
 )
 VALUES
     (
+        'Hershey King',
         'Hershey',
         3.00,
         'King',
         'Chocolate'
     ),
     (
+        'Hershey Regular',
         'Hershey',
         1.00,
         'Regular',
         'Chocolate'
     ),
     (
+        'Jolly Ranchers',
         'Jolly Rancher',
-        1.20,
+        3.20,
         'Regular',
         'Fruity'
     ),
     (
+        'Skittles Share',
         'Skittles',
         3.00,
         'Share',
         'Fruity'
     ),
     (
+        'Skittles Regular',
         'Skittles',
         1.00,
         'Regular',
         'Fruity'
     ),
     (
+        'KitKat Minis',
         'KitKat',
         5.00,
         'Minis',
         'Chocolate'
     ),
     (
+        'KitKat King',
         'KitKat',
-        1.00,
+        3.00,
         'King',
         'Chocolate'
     ),
     (
+        'KitKate Regular',
         'KitKat',
-        3.00,
+        1.00,
         'Regular',
         'Chocolate'
     ),
     (
+        'KitKat Dark Chocolate',
         'KitKat',
         1.25,
         'Regular',
         'Dark Chocolate'
     ),
     (
-        'KitKat',
-        3.25,
-        'King',
-        'Dark Chocolate'
-    ),
-    (
+        'M&Ms',
         'M&M',
         1.00,
         'Regular',
         'Chocolate'
     ),
     (
-        'M&M',
-        3.00,
-        'Share',
-        'Chocolate'
-    ),
-    (
-        'M&M',
-        1.25,
-        'Regular',
-        'Caramel'
-    ),
-    (
+        'M&Ms Peanuts',
         'M&M',
         1.25,
         'Regular',
         'Peanut'
-    ),
-    (
-        'M&M',
-        3.25,
-        'Share',
-        'Caramel'
-    ),
-    (
-        'M&M',
-        3.25,
-        'Share',
-        'Peanut'
-    ),
-    (
-        'Snickers',
-        1.00,
-        'Regular',
-        'Caramel'
-    ),
-    (
-        'Snickers',
-        3.00,
-        'Share',
-        'Caramel'
-    ),
-    (
-        'Nerds',
-        1.50,
-        'Regular',
-        'Fruity'
-    ),
-    (
-        'Twix',
-        0.75,
-        'Regular',
-        'Caramel'
     );
